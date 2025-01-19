a8a41c33e279e1ac174c3e173a10d8e94294c50b
AlterThego <theosuaking@gmail.com>
2025-01-19 12:13:12 +0800

    Improved contents

--- assets/js/about-me-script.js
+++ assets/js/about-me-script.js
@@ -1,60 +1,46 @@
 const data = [
     {
         education: `
-            <p class="text-cyan-400 font-bold">searching database. </p>
-            <p class="text-cyan-400 font-bold">preparing for education information rendering. </p>
-            <p class="text-green-500 font-bold">ready for access. </p>
-            >> database\\PLR-7H8J-K4L5-N2M9\\Education: <br>&nbsp[<br> 
-            &nbsp&nbsp--nursery-school: Baguio Cathedral Nursery School; <br> 
-            &nbsp&nbsp--elementary: Saint Louis School (Center), ED; <br> 
-            &nbsp&nbsp--high-school: Saint Louis School (Center), HD; <br> 
-            &nbsp&nbsp--senior-high-school: Saint Louis School (Center), HD; <br> 
-            &nbsp&nbsp--college-br: University of Cordilleras, Inc.<br> 
-            &nbsp&nbsp--course:<br>
-            <p class="text-[--primary-yellow] text-center font-bold">Bachelor of Science in</p>
-            <p class="text-[--primary-yellow] text-center font-bold">Information Technology</p>
-            <p class="text-[--primary-yellow] text-center font-bold">Web Technologies Track</p>]<br> 
-            <p class="text-green-600 text-center font-bold">Data compilation successful.</p>`
+            <p class="text-cyan-400 font-bold uppercase">searching database. </p>
+            <p class="text-cyan-400 font-bold uppercase">preparing for education information rendering. </p>
+            <p class="text-green-500 font-bold uppercase">ready for access. </p>
+            >> DATABASE\\PLR-7H8J-K4L5-N2M9\\Education: <br>&nbsp[<br> 
+            <p class="px-4">ELEMENTARY: Saint Louis School (Center), Elementary Department; </p>
+            <p class="px-4">JUNIOR-HIGHSCHOOL: Saint Louis School (Center), Highschool Department; </p>
+            <p class="px-4">SENIOR-HIGH-SCHOOL: Saint Louis School (Center), Highschool Department; </p>
+            <p class="px-4">COLLEGE: University of Cordilleras, Inc.</p>
+            <p class="px-4">COURSE: </p>
+            <p class="text-[--primary-yellow] text-center font-bold uppercase">Bachelor of Science in</p>
+            <p class="text-[--primary-yellow] text-center font-bold uppercase">Information Technology</p>
+            <p class="text-[--primary-yellow] text-center font-bold uppercase">Web Technologies Track</p>]<br> 
+            <p class="text-green-600 text-center font-bold uppercase">Data compilation successful.</p>`
     },
     {
         experience: `
-            <p class="text-cyan-400 font-bold">searching database. </p>
-            <p class="text-cyan-400 font-bold">preparing for experience information rendering.</p>
-            <p class="text-green-500 font-bold">ready for access. </p>
-            >> database\\PLR-7H8J-K4L5-N2M9\\Experience: <br>&nbsp[<br> 
-            <p class="text-red-500 font-bold">&nbsp&nbspERROR: Not applicable yet!</p> ]
-            <p class="text-green-600 text-center font-bold">Data compilation successful.</p>`
+            <p class="text-cyan-400 font-bold uppercase">searching database. </p>
+            <p class="text-cyan-400 font-bold uppercase">preparing for experience information rendering.</p>
+            <p class="text-green-500 font-bold uppercase">ready for access. </p>
+            >> DATABASE\\PLR-7H8J-K4L5-N2M9\\Experience: <br>&nbsp[<br> 
+            <p class="px-4">ACCENTURE: SAP Advanced Business Application Programming (ABAP)</p> ]
+            <p class="text-green-600 text-center font-bold uppercase">Data compilation successful.</p>`
     },
     {
-        projects: `>> database\\PLR-7H8J-K4L5-N2M9\\Projects: [<br> 
-            <p class="text-cyan-400 font-bold">searching database. </p>
-            <p class="text-cyan-400 font-bold">preparing for project information rendering. </p>
-            <p class="text-green-500 font-bold">ready for access. </p>
+        projects: `
+            <p class="text-cyan-400 font-bold uppercase">searching database. </p>
+            <p class="text-cyan-400 font-bold uppercase">preparing for project information rendering. </p>
+            <p class="text-green-500 font-bold uppercase">ready for access. </p>
+            >> DATABASE\\PLR-7H8J-K4L5-N2M9\\Projects: [<br> 
             <p><a href="#"></a>1. Personal Portfolio Website</p>
             <a href="https://github.com/AlterThego/PVO-EcoMetrics" target="_blank">2. Socio Economic Profile of Benguet</p> ]<br>
-            <p class="text-green-600 text-center font-bold">Data compilation successful.</p>`
+            <p class="text-green-600 text-center font-bold uppercase">Data compilation successful.</p>`
     },
-    {
-        skills: `
-            <p class="text-cyan-400 font-bold">searching database. </p>
-            <p class="text-cyan-400 font-bold">preparing for skill information rendering. </p>
-            <p class="text-green-500 font-bold">ready for access. </p>
-            >> database\\PLR-7H8J-K4L5-N2M9\\Skills: [<br> 
-            --programming-languages: JavaScript, PHP, HTML, CSS, SAP ABAP<br> 
-            --frameworks-Libraries: Laravel, Node.js, TailwindCSS<br> 
-            --databases: MySQL, SQLite<br> 
-            --tools: Git, Visual Studio Code, Github<br> 
-            --soft-skills: Problem-solving, Critical Thinking, Communication <br> ]
-            <p class="text-green-600 text-center font-bold">Data compilation successful.</p>`
-    }
-    
 ];
 
 const allElements = document.getElementsByClassName("typeing");
 const element = document.getElementById("AboutDevTypeText");
 const clearButton = document.querySelector('.clear-btn');
 
-let currentContent = ">>> start by pressing any of the buttons...";
+let currentContent = ">>> START BY PRESSING ANY OF THE BUTTONS...";
 let i = 0, isTag, text;
 let isTerminalEmpty = false;
 
@@ -81,8 +67,8 @@ let isTerminalEmpty = false;
 
 function clearTerminal() {
     if (!isTerminalEmpty) {
-        element.innerHTML = '>>> start by pressing any of the buttons...';
-        currentContent = '>>> start by pressing any of the buttons...';
+        element.innerHTML = '>>> START BY PRESSING ANY OF THE BUTTONS...';
+        currentContent = '>>> START BY PRESSING ANY OF THE BUTTONS...';
         i = 0;
         isTerminalEmpty = true;
     }
